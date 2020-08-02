48ac0e9d3857b2cb6dae0eeeb3cc10afa9ca7bda
Gaurav Mendse <gauravmendse@Gauravs-MacBook-Pro.local>
2020-08-02 13:06:23 +0530

    created new auth module, moved auth related components and dependant imports from apps module to this, finally import this module in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 /** Custom Angular Modules */
 import { AppRoutingModule } from './app-routing.module';
 import { PostsModule } from './posts/posts.module';
-
+import { AuthModule } from './auth/auth.module';
 /** Angular Material Module */
 import { AngularMaterialModule } from './angular-material.module';
 
 /** Components */
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { ErrorComponent } from './error/error.component';
 
 /** HTTP Interceptors */
@@ -24,21 +21,15 @@ import { AuthInterceptor } from './auth/auth.interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    SignupComponent,
-    ErrorComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, ErrorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
     PostsModule,
+    AuthModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
