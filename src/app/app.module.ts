404efd7fe22f7f2b7dc2d59f17122726fe9f138b
Gaurav Mendse <gauravmendse@Gauravs-MacBook-Pro.local>
2020-08-02 12:51:53 +0530

    created separate modules for angular-material and posts components, and call them in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 /** Custom Angular Modules */
 import { AppRoutingModule } from './app-routing.module';
+import { PostsModule } from './posts/posts.module';
 
-/** Angular Material Modules */
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
+/** Angular Material Module */
+import { AngularMaterialModule } from './angular-material.module';
 
 /** Components */
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ErrorComponent } from './error/error.component';
@@ -35,8 +27,6 @@ import { ErrorInterceptor } from './error-interceptor';
   declarations: [
     AppComponent,
     HeaderComponent,
-    PostCreateComponent,
-    PostListComponent,
     LoginComponent,
     SignupComponent,
     ErrorComponent,
@@ -44,18 +34,11 @@ import { ErrorInterceptor } from './error-interceptor';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule,
+    AngularMaterialModule,
+    PostsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
